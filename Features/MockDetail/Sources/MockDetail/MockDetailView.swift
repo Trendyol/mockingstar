5097420ec33ff8c8da05de7d58b6a355b8f18550
Yusuf Özgül <yusuf.ozgul@trendyol.com>
2025-03-27 16:29:19 +0300

    Add navigation support for move-copy-duplicate mock buttons

--- Features/MockDetail/Sources/MockDetail/MockDetailView.swift
+++ Features/MockDetail/Sources/MockDetail/MockDetailView.swift
@@ -79,7 +79,9 @@ public struct MockDetailView: View {
 
                     Menu("Duplicate...") {
                         Button {
-                            Task { await viewModel.duplicateMock() }
+                            Task {
+                                await viewModel.newMock(mockDomain: viewModel.mockDomain, shouldMove: false)
+                            }
                         } label: {
                             Label("Duplicate Mock", systemImage: "doc.on.doc.fill")
                         }
@@ -88,7 +90,9 @@ public struct MockDetailView: View {
                     Menu("Copy to...") {
                         ForEach(domainDiscover.domains.filter { $0 != viewModel.mockDomain }, id: \.self) { mockDomain in
                             Button(mockDomain) {
-                                Task { await viewModel.copyMock(to: mockDomain) }
+                                Task {
+                                    await viewModel.newMock(mockDomain: mockDomain, shouldMove: false)
+                                }
                             }
                         }
                     }
@@ -97,7 +101,7 @@ public struct MockDetailView: View {
                         ForEach(domainDiscover.domains.filter { $0 != viewModel.mockDomain }, id: \.self) { mockDomain in
                             Button(mockDomain) {
                                 Task {
-                                    await viewModel.moveMock(to: mockDomain)
+                                    await viewModel.newMock(mockDomain: mockDomain, shouldMove: true)
                                 }
                             }
                         }
@@ -120,11 +124,11 @@ public struct MockDetailView: View {
             Button("Delete", role: .destructive, action: { viewModel.removeMock() })
             Button("Cancel", role: .cancel, action: { })
         }
-        .alert("Error", isPresented: $viewModel.shouldShowSaveErrorAlert, actions: {
-            Button("Discard Changes", role: .destructive, action: { viewModel.discardChanges() })
+        .alert(viewModel.alertMessage, isPresented: $viewModel.shouldShowAlert, actions: {
+            if let action = viewModel.alertAction {
+                Button(viewModel.alertActionTitle) { action() }
+            }
             Button("Cancel", role: .cancel, action: { })
-        }, message: {
-            Text(viewModel.saveErrorMessage)
         })
         .alert("Warning", isPresented: $viewModel.shouldShowFilePathErrorAlert, actions: {
             Button("Fix File Path", role: .destructive, action: { viewModel.fixFilePath() })
@@ -139,10 +143,8 @@ public struct MockDetailView: View {
         .navigationDestination(isPresented: $shouldShowMockReloadView) {
             MockReloadView(viewModel: .init(mockModel: viewModel.mockModel, mockDomain: viewModel.mockDomain))
         }
-        .onChange(of: viewModel.shouldDismissView) { _, _ in
-            guard viewModel.shouldDismissView else { return }
-            withAnimation { dismiss() }
-        }
+        .onChange(of: viewModel.shouldDismissView) { dismissIfNeeded() }
+        .onChange(of: viewModel.shouldShowAlert) { dismissIfNeeded() }
         .task(id: viewModel.mockModel.responseBody) { viewModel.jsonEditorModelTypeChanged() }
         .task(id: viewModel.mockModel.responseHeader) { viewModel.jsonEditorModelTypeChanged() }
         .task(id: viewModel.mockModel.metaData) { viewModel.checkUnsavedChanges() }
@@ -156,8 +158,9 @@ public struct MockDetailView: View {
         }
     }
 
-    func dismiss() {
-        navigationStore.pop()
+    func dismissIfNeeded() {
+        guard viewModel.shouldDismissView && !viewModel.shouldShowAlert else { return }
+        withAnimation { navigationStore.pop() }
     }
 }
 
