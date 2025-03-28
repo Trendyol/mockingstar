202f9bb12470457012e492fe6f904e90fcc8e22c
Yusuf Özgül <yusuf.ozgul@trendyol.com>
2025-03-28 14:51:06 +0300

    Fix build

--- Core/Server/Sources/Server/Utils/ServerLogger.swift
+++ Core/Server/Sources/Server/Utils/ServerLogger.swift
@@ -10,26 +10,25 @@ import CommonKit
 import Foundation
 import FlyingSocks
 
-extension Logger: Logging, @unchecked Sendable {
-    public func logDebug(_ debug: @autoclosure () -> String) {
-        self.debug(debug())
+extension Logger: @retroactive Logging, @unchecked @retroactive Sendable {
+    public func logDebug(_ debug: String) {
+        self.debug(debug)
     }
     
-    public func logInfo(_ info: @autoclosure () -> String) {
-        let info = info()
+    public func logInfo(_ info: String) {
         guard !info.contains("close connection") && !info.contains("open connection") else { return }
         self.info(info)
     }
     
-    public func logWarning(_ warning: @autoclosure () -> String) {
-        self.warning(warning())
+    public func logWarning(_ warning: String) {
+        self.warning(warning)
     }
     
-    public func logError(_ error: @autoclosure () -> String) {
-        self.error(error())
+    public func logError(_ error: String) {
+        self.error(error)
     }
     
-    public func logCritical(_ critical: @autoclosure () -> String) {
-        self.critical(critical())
+    public func logCritical(_ critical: String) {
+        self.critical(critical)
     }
 }
