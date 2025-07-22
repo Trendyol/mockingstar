8cf8418e21133d4bba86fb53dc9b72b2301e3101
Yusuf Özgül <yusuf.ozgul@trendyol.com>
2025-07-22 09:44:51 +0300

    Add mock usage command

--- MockingStarExecutable/Sources/MockingStar.swift
+++ MockingStarExecutable/Sources/MockingStar.swift
@@ -13,7 +13,7 @@ import ArgumentParser
 struct MockingStar: AsyncParsableCommand {
     static var configuration = CommandConfiguration(
         abstract: "MockingStar powerful mock server",
-        subcommands: [Start.self],
+        subcommands: [Start.self, MockUsageAnalyzer.self],
         defaultSubcommand: Start.self)
 }
 
@@ -44,6 +44,46 @@ private struct Start: AsyncParsableCommand {
     }
 }
 
+private struct MockUsageAnalyzer: AsyncParsableCommand {
+    static var configuration = CommandConfiguration(
+        commandName: "analyze-usage",
+        abstract: "Analyze previous mock usage from logs file")
+
+    @Option(name: .shortAndLong, help: "Logs folder")
+    var logsFolder: String
+
+    @Flag(name: .shortAndLong, help: "Show Detailed Usage")
+    var verbose: Bool = false
+
+    func run() async throws {
+        let fileURL: URL
+
+        let customFolderExist = FileManager.default.fileOrDirectoryExists(atPath: logsFolder)
+        if customFolderExist.isExist && customFolderExist.isDirectory {
+            fileURL = URL(filePath: logsFolder).appending(path: "MockingStar.log")
+        } else if customFolderExist.isExist {
+            fileURL = URL(filePath: logsFolder)
+        } else {
+            fatalError("Custom mocks folder not exist.")
+        }
+
+        guard FileManager.default.fileOrDirectoryExists(atPath: fileURL.path()).isExist else {
+            fatalError("Logs file not exist.")
+        }
+
+        var mockUsage: [String: Int] = [:]
+
+        for try await line in fileURL.lines where line.contains("Mock Found:") {
+            guard let id = line.components(separatedBy: " ").last else { continue }
+            mockUsage[id] = mockUsage[id, default: 0] + 1
+        }
+
+        print("Total mock usage", mockUsage.values.reduce(0, +))
+
+        if verbose {
+            let sortedMocks = mockUsage.sorted { $0.value > $1.value }
+            for (mockId, count) in sortedMocks {
+                print("Mock ID: \(mockId) - usage: \(count)")
             }
         }
     }
